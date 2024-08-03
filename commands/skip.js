69364bdc7b759f34a06672d0109b49fb0a0bc3a1
bomie <bomidelrey@gmail.com>
2024-08-03 20:47:12 +0700

    skip.js

--- commands/skip.js
+++ commands/skip.js
@@ -30,7 +30,7 @@ module.exports = {
 
     if (!queue) {
       const embed = new EmbedBuilder()
-        .setColor('#FF0000')
+        .setColor('#812f49')
         .setAuthor({ 
           name: 'Alert!', 
           iconURL: musicIcons.dotIcon,
@@ -42,7 +42,7 @@ module.exports = {
 
     if (queue.songs.length <= 1) {
       const embed = new EmbedBuilder()
-        .setColor('#FF0000')
+        .setColor('#812f49')
         .setAuthor({ 
           name: 'Alert!', 
           iconURL: musicIcons.dotIcon,
@@ -55,7 +55,7 @@ module.exports = {
     queue.skip();
 
     const embed = new EmbedBuilder()
-      .setColor('#00FF00')
+      .setColor('#bc8787')
       .setAuthor({ 
         name: lang.skipTitle, 
         iconURL: musicIcons.skipIcon,
