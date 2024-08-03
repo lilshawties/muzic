eb9fab0e0bb7e860333d11f57fcdac60ce3e6e6a
bomie <bomidelrey@gmail.com>
2024-08-03 20:47:37 +0700

    stop.js

--- commands/stop.js
+++ commands/stop.js
@@ -30,7 +30,7 @@ module.exports = {
 
     if (!queue) {
       const embed = new EmbedBuilder()
-        .setColor('#FF0000')
+        .setColor('#812f49')
         .setAuthor({ 
           name: 'Alert!', 
           iconURL: musicIcons.dotIcon,
@@ -43,7 +43,7 @@ module.exports = {
     queue.stop();
 
     const embed = new EmbedBuilder()
-      .setColor('#FF0000')
+      .setColor('#bc8787')
       .setAuthor({ 
         name: lang.stopTitle, 
         iconURL: musicIcons.stopIcon,
