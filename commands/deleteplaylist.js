ca069b71aba88329c15f7d1b9a56ba6f0ba12368
bomie <bomidelrey@gmail.com>
2024-08-03 20:34:04 +0700

    deleteplaylist.js

--- commands/deleteplaylist.js
+++ commands/deleteplaylist.js
@@ -50,7 +50,7 @@ module.exports = {
         }
 
         const embed = new EmbedBuilder()
-          .setColor('#00FF00')
+          .setColor('#bc8787')
           .setAuthor({ 
             name: lang.successTitle, 
             iconURL: musicIcons.correctIcon,
